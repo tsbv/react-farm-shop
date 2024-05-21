14670c1bf885481725f469b51c96db876a2ae7c0
Georgyi Tsabiev <gtsabiev@inbox.ru>
2024-05-21 17:42:42 +0300

    Реализует вёрстку блока about

--- src/components/ui/title/title.jsx
+++ src/components/ui/title/title.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import "./style.css";
 
-function Title() {
-  return <></>;
+export const TitleSize = {
+  SMALL: "small",
+  BIG: "big",
+  DEFAULT: ""
+};
+
+function Title({ children, size }) {
+  return (
+    <h1 className={`title${size ? ` title_${size}` : ""}`}>
+      {children}
+    </h1>
+  ); 
 }
 
 export default Title;
