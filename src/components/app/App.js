c0b0d2784d8ede817b3d9b59204411d82c6509e7
Georgyi Tsabiev <gtsabiev@inbox.ru>
2024-06-04 17:48:42 +0300

    Добавляет стили темы приложения и глобальные стили

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import PageWrapper from "../layout/page-wrapper/page-wrapper";
-import "./style.css";
+import "./style.js";
 import advantagesList from "../../mocks/advantagesList";
+import { GlobalStyle } from "./style";
 
 export default function App() {
-  return <PageWrapper advantages={advantagesList} />;
+  return (
+    <>
+      <GlobalStyle />
+      <PageWrapper advantages={advantagesList} />
+    </>
+  );
 }
---
+++ src/components/app/style.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  html {
+    height: 100%;
+  }
+
+  body,
+  html {
+    margin: 0;
+  }
+
+  body {
+    position: relative;
+    min-height: 100%;
+    font-family: ${(props) => props.theme.fontFamily};
+    font-size: ${(props) => props.theme.fontSizeDefault};
+    line-height: 1.5;
+    font-weight: 400;
+    color: ${(props) => props.theme.colorBlackForText};
+  }
+`;
