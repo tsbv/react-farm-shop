f0acac7b6afe6e022d740f0cb88516056a930730
Georgyi Tsabiev <gtsabiev@inbox.ru>
2024-06-04 17:34:25 +0300

    Добавляет директорию themes с повторяющимися значениями свойств

---
+++ src/themes/default.js
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+  colorBlackForText: "#333333",
+  colorWhite: "#ffffff",
+  colorGray: "#f6f6f6",
+  colorForLightBackground: "#d8ecfe",
+  colorForCircleBackground: "#c4e2ff",
+  colorForFarmCategory: "#88aa4d",
+  colorForStoreCategory: "#f75531",
+  colorForButton: "#fc9b27",
+  colorForButtonHover: "#fc7427",
+  pagePadding: "90px",
+  fontFamily: '"Inter", "Arial", sans-serif',
+  fontSizeDefault: "18px",
+  pageWidth: "1280px"
+};
